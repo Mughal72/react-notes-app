de3d803a3e52df9dee383141351d8b28b8f49b27
Anber <ambersattarmh@gmail.com>
2024-02-19 16:30:41 +0500

    first commit

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -2,23 +2,24 @@ import Note from './Note';
 import AddNote from './AddNote';
 
 const NotesList = ({
-	notes,
-	handleAddNote,
-	handleDeleteNote,
+    notes,
+    handleAddNote,
+    handleDeleteNote,
 }) => {
-	return (
-		<div className='notes-list'>
-			{notes.map((note) => (
-				<Note
-					id={note.id}
-					text={note.text}
-					date={note.date}
-					handleDeleteNote={handleDeleteNote}
-				/>
-			))}
-			<AddNote handleAddNote={handleAddNote} />
-		</div>
-	);
+    return (
+        <div className='notes-list'>
+            {notes.map((note) => (
+                <Note
+                    key={note.id} // Add a unique key prop here
+                    id={note.id}
+                    text={note.text}
+                    date={note.date}
+                    handleDeleteNote={handleDeleteNote}
+                />
+            ))}
+            <AddNote handleAddNote={handleAddNote} />
+        </div>
+    );
 };
 
 export default NotesList;
